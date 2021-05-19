653b267c5b3363f06326c671c55515f391d3ce64
Nicolás Gargano <nicolasgargano@gmail.com>
2021-05-19 10:07:23 -0300

    Add tweets demo in sandbox.
    One at the start, and the corresponding one for SubmissionChallenged events.

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -2,7 +2,9 @@ import { ethers } from "ethers"
 import { ProofOfHumanity__factory } from "../generated/poh-ethers-contracts/factories/ProofOfHumanity__factory"
 import { AppConfig } from "../src/config"
 import * as config from "../src/config"
-import { getChallengeInfo } from "./poh"
+import { ChallengeInfo, getChallengeInfo } from "./poh"
+import * as tweet from "./tweet"
+import { NewChallengeTweetData } from "./tweet"
 
 async function main(configuration: AppConfig) {
   console.info("Starting with configuration:", configuration)
@@ -12,8 +14,27 @@ async function main(configuration: AppConfig) {
 
   const poh = ProofOfHumanity__factory.connect(config.pohContractAddress, provider)
   const santiAddress = "0x2a52309edf998799c4a8b89324ccad91848c8676"
-  const info = await poh.getSubmissionInfo(santiAddress)
-  console.info("Got submission info:", info)
+  const santiInfo = await poh.getSubmissionInfo(santiAddress)
+  console.info("Got submission info:", santiInfo)
+
+  const infoToTweetData = (challengeInfo: ChallengeInfo) => {
+    const tweetData: NewChallengeTweetData = {
+      name: challengeInfo.name,
+      reasonGiven: challengeInfo.challengeReason,
+      pohProfileUrl: challengeInfo.pohProfileLink,
+      klerosCaseUrl: challengeInfo.klerosCaseLink
+    }
+    return tweetData
+  }
+
+  const demoChallengeInfo = await getChallengeInfo(
+    "0xF8fD6890836e2051112B749a9f981b0827050a47".toLowerCase(),
+    0,
+    0
+  )
+  const tweetStatus = tweet.makeStatus(infoToTweetData(demoChallengeInfo))
+  console.info("== Demo challenge tweet ==")
+  console.log(tweetStatus)
 
   poh.on(poh.filters.AddSubmission(), (_, __, event) => {
     console.info("Got submission add event:", event)
@@ -26,7 +47,8 @@ async function main(configuration: AppConfig) {
       event.args._requestID.toNumber(),
       event.args._challengeID.toNumber()
     )
-    console.info("Info", info)
+    console.info("Challenge info", info)
+    console.info("Tweet status", tweet.makeStatus(infoToTweetData(info)))
   })
 }
 
