ab8f774de2a7a86f987e3849af2c5743424cd727
Nicolás Gargano <nicolasgargano@gmail.com>
2021-05-19 08:08:37 -0300

    Improve readability in poh requests

--- src/poh.ts
+++ src/poh.ts
@@ -3,7 +3,7 @@ import {
   pohApi_URL,
   pohProfile_BASEURL,
   klerosCase_BASEURL,
-  ipfs_BASEURL,
+  ipfs_BASEURL
 } from "./config"
 
 type ChallengeInfo = {
@@ -18,24 +18,6 @@ type Evidence = {
   description: string
 }
 
-const infoQuery = (submissionId: string) => `
-    query {
-        submission(id: "${submissionId}") {
-            name
-            requests {
-              type
-              evidence(orderBy:creationTime orderDirection: asc) {
-                URI
-              }
-              challenges {
-                reason
-                challengeID
-                disputeID
-              }
-            }
-        }
-    }
-`
 
 export const getChallengeInfo = async (
   submissionId: string,
@@ -46,24 +28,43 @@ export const getChallengeInfo = async (
     url: pohApi_URL,
     method: "post",
     data: {
-      query: infoQuery(submissionId),
-    },
+      query: infoQuery(submissionId)
+    }
   })
 
   const submission = res.data.data.submission
-
   const evidenceURI = submission.requests[requestId].evidence[1].URI
-
-  const fullReason: Evidence = (
-    await axios.get(`${ipfs_BASEURL}${evidenceURI}`)
-  ).data
+  const evidenceRes = await axios.get(`${ipfs_BASEURL}${evidenceURI}`)
+  const fullReason = evidenceRes.data
 
   const challengeInfo: ChallengeInfo = {
     name: submission.name,
     challengeReason: fullReason.description,
     pohProfileLink: `${pohProfile_BASEURL}${submissionId.toLowerCase()}`,
-    klerosCaseLink: `${klerosCase_BASEURL}${submission.requests[requestId].challenges[challengeId].disputeID}`,
+    klerosCaseLink: `${klerosCase_BASEURL}${submission.requests[requestId].challenges[challengeId].disputeID}`
   }
 
   return challengeInfo
 }
+
+
+// -- HELPERS
+
+
+const infoQuery = (submissionId: string) => `
+    query {
+        submission(id: "${submissionId}") {
+            name
+            requests {
+              type
+              evidence(orderBy:creationTime, orderDirection: asc) {
+                URI
+              }
+              challenges {
+                reason
+                challengeID
+                disputeID
+              }
+            }
+        }
+    }`
