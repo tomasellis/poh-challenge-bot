da300a60b94bc72c116bb7dfba0a6b7c0889a1a5
Tomás Ellis <tomasitoellis@gmail.com>
2021-05-19 05:06:10 -0300

    Add function to fetch challenge information

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,8 @@ export const appConfig: AppConfig = {
 }
 
 export const pohContractAddress = "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb"
+export const pohApi_URL =
+  "https://api.thegraph.com/subgraphs/name/kleros/proof-of-humanity-mainnet"
+export const klerosCase_BASEURL = "https://resolve.kleros.io/cases/"
+export const pohProfile_BASEURL = "https://app.proofofhumanity.id/profile/"
+export const ipfs_BASEURL = "https://ipfs.io/"
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,24 +1,27 @@
 import { ethers } from "ethers"
 import { ProofOfHumanity__factory } from "../generated/poh-ethers-contracts/factories/ProofOfHumanity__factory"
 import { appConfig, AppConfig, pohContractAddress } from "../src/config"
+import { getChallengeInfo } from "./poh"
 
 async function main(configuration: AppConfig) {
   console.info("Starting with configuration:", configuration)
 
   const provider = new ethers.providers.JsonRpcProvider(configuration.infuraURL)
+  provider.pollingInterval = 60 * 1000
   const poh = ProofOfHumanity__factory.connect(pohContractAddress, provider)
 
-  const santiAddress = "0x2a52309edf998799c4a8b89324ccad91848c8676"
-  const info = await poh.getSubmissionInfo(santiAddress)
-  console.info("Got submission info:", info)
-
   poh.on(poh.filters.AddSubmission(), (_, __, event) => {
-    console.info("Got submission add event:", event)
+    console.info(`Got submission at ${Date.now() / 3600000} add event:`, event)
   })
 
   poh.on(poh.filters.SubmissionChallenged(), (_, __, ___, event) => {
     console.info("Got submission challenge event:", event)
   })
+
+  // Usage of getSubmissionInfo
+  console.info(
+    await getChallengeInfo("0xe0da208c5c29d1e89b16c018303b02a89709a1ef", 1, 0)
+  )
 }
 
 main(appConfig)
